710f8707dc522f3b72a34a2f5c4e44288823926c
jason <JZelmano@mail.yu.edu>
2023-02-26 15:09:55 -0500

    added HasTable Test code & skeletons for all stages

--- DataStructures/project/stage1/src/test/java/edu/yu/cs/com1320/project/impl/HashTableImplTest.java
+++ DataStructures/project/stage1/src/test/java/edu/yu/cs/com1320/project/impl/HashTableImplTest.java
@@ -34,6 +34,88 @@ package edu.yu.cs.com1320.project.impl;
          assertEquals(s,"UpdatedHello");
      }
 
+     /*Test for collisions - ensure base array (size 5) using separate chaining is functioning properly
+       1) put 100 <integer,integer> into a HashTable Impl
+       2) key will be a 1 to 100, Value will be key + 18
+       3) for loop assigning values and ensuring return value is null for each put
+       4) for loop getting values and ensuring return value == key + 18
+       5) update keys for value == key + 36 - check for old return value in put and new value in get
+      */
+     @Test
+     void collisionTestingInteger(){
+        for(int i = 1; i <= 100; i++){
+            assertNull(c.put(i,i+18));
+        }
+        for(int i = 1; i <= 100; i++){
+            assertEquals(c.get(i),i+18);
+        }
+        for(int i = 1; i <= 100; i++){
+            assertEquals(c.put(i,i+36),i+18);
+        }
+        for(int i = 1; i <= 100; i++){
+            assertEquals(c.get(i),i+36);
+        }
+     }
+     /*
+     1)
+      */
+     @Test
+     void collisionTestingString() {
+         int count = 0;
+         for(int i = 65; i <= 88; i++){
+             count++;
+             String k = "";
+             String v = "";
+             for(int j = i; j <= i+2; j++){
+                 k += (char) j;
+             }
+             for(int j = i+2; j >= i; j--){
+                 v += (char) j;
+             }
+             assertNull(f.put(k,v));
+         }
+
+         for(int i = 97; i <= 120; i++){
+             count++;
+             String k = "";
+             String v = "";
+             for(int j = i; j <= i+2; j++){
+                 k += (char) j;
+             }
+             for(int j = i+2; j >= i; j--){
+                 v += (char) j;
+             }
+             assertNull(f.put(k,v));
+         }
+
+         for(int i = 65; i <= 87; i++){
+             String k = "";
+             String v = "";
+             for(int j = i; j <= i+2; j++){
+                 k += (char) j;
+             }
+             for(int j = i+2; j >= i; j--){
+                 v += (char) j;
+             }
+             assertEquals(f.get(k),v);
+             System.out.println("Key: " + k + ", Value: " + v);
+         }
+
+         for(int i = 97; i <= 120; i++){
+             String k = "";
+             String v = "";
+             for(int j = i; j <= i+2; j++){
+                 k += (char) j;
+             }
+             for(int j = i+2; j >= i; j--){
+                 v += (char) j;
+             }
+             assertEquals(f.get(k),v);
+             System.out.println("Key: " + k + ", Value: " + v);
+         }
+         System.out.println(count);
+     }
+
      @Test
      void get() {
          c.put(19,"Jason");
